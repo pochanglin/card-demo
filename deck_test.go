09fab914fd263adb7552fda23e0723c52f50d469
PoChangLin <allen4353968@gmail.com>
2021-01-26 13:10:06 +0800

    add deck.go deck_test.go

---
+++ deck_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected length 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
